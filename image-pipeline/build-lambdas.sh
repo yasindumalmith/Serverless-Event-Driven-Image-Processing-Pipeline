5346dd6bb69dc2472b47a4a884e05e61c5b260c7
yasindumalmith <yasindumalmith058@gmail.com>
2026-05-16 05:03:41 +0530

    create zip files

---
+++ image-pipeline/build-lambdas.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+LAMBDAS=("presign" "status")
+
+for lambda in "${LAMBDAS[@]}"; do
+  echo "Building $lambda..."
+  cd lambdas/$lambda/src
+  npm install --production
+  cd ..
+  rm -f $lambda.zip
+  cd src
+  zip -r ../$lambda.zip . -x "*.git*"
+  cd ../../..
+  echo "Built lambdas/$lambda/$lambda.zip"
+done
+
+echo "All Lambdas built successfully"
